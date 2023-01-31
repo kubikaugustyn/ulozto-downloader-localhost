b6e699080282ed3b1773a6a71a806a9be96896d5
Jakub Augustýn <kubik.augustyn@post.cz>
2023-01-31 17:28:02 +0100

    Made change to way of connecting, upgraded code I was connecting to wss://localhost..., but ws://localhost..., it's necessary to not send HTTPS handshake. at 31. 1. 2023 5:28PM

--- ulozto-downloader-localhost/Server.py
+++ ulozto-downloader-localhost/Server.py
@@ -1,7 +1,10 @@
 #  -*- coding: utf-8 -*-
 __author__ = "kubik.augustyn@post.cz"
 
+import base64
+import hashlib
 import socket
+import uuid
 from threading import Thread
 
 
@@ -26,15 +29,60 @@ class Server:
     def __handleConnection(self, conn, addr):
         # print(conn, addr)
         # https://en.wikipedia.org/wiki/WebSocket
+        id = uuid.uuid4()
         allData = b''
+        message = b''
+        messages = []
         while True:
             data = conn.recv(4096)
-            if not data or data == b'':
+            if not data:
                 break
             allData += data
             print(data)
+            intData = list(data)
+            """while len(intData) > 2:
+                firstByte = intData.pop(0)
+                fin = firstByte & 0x80
+                rsv = firstByte & 0x40
+                rsv <<= firstByte & 0x20
+                rsv <<= firstByte & 0x10
+                opcode = firstByte & 0x08
+                opcode <<= firstByte & 0x04
+                opcode <<= firstByte & 0x02
+                opcode <<= firstByte & 0x01
+                length = intData.pop(0)
+                mask = length & 0x80 > 1
+                if mask:
+                    length -= 0x80
+                if length > 125:
+                    if length == 126:
+                        size = 2
+                    elif length == 127:
+                        size = 8
+                    else:
+                        raise RuntimeError("Something went very wrong.")
+                    length = 0
+                    for _ in range(size):
+                        length <<= 8
+                        length += intData.pop(0)
+                maskingKey = 0
+                if mask:
+                    for _ in range(4):
+                        maskingKey <<= 8
+                        mask += intData.pop(0)
+                print(f"({id}) Got frame (mask: {maskingKey}, len: {length}):", bytes(intData[:length]))"""
+            if not data.decode('ascii').startswith("GET /hahaha"):
+                conn.close()
+                return
+            sha1 = hashlib.sha1()
+            sha1.update(base64.b64decode(input("Tell us the base64: ")))
+            sha1.update(bytes("258EAFA5-E914-47DA-95CA-C5AB0DC85B11", 'ascii'))
+            print("Hashed:", sha1.digest(), f"===> {base64.b64encode(sha1.digest()).decode('ascii')}")
             conn.send(bytes(
-                "HTTP/1.1 101 Switching Protocols\nUpgrade: websocket\nConnection: Upgrade\nSec-WebSocket-Accept: HSmrc0sMlYUkAGmm5OPpG2HaGWk=\nSec-WebSocket-Protocol: chat",
+                f"HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {base64.b64encode(sha1.digest()).decode('ascii')}\r\nSec-WebSocket-Protocol: chat\r\n\r\n",
                 'utf-8'))
-        print("Ahhhhhhhhhh")
-        print("Received:", allData)
+            print("Sent response!")
+        print(f"({id}) Connection closed. Received:", allData)
+        with open(f"captures/{id}.bin", "wb+") as f:
+            f.write(allData)
+            f.close()
